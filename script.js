9272b7b2890efc72d6abecc5f3eead3ff490d42f
hsouporto <hmso.lsa@gmail.com>
2026-04-10 11:48:57 +0100

    first template commit

--- script.js
+++ script.js
@@ -2,27 +2,32 @@ async function loadPublications() {
   const container = document.getElementById("pubs");
 
   try {
-    const res = await fetch("publications.json");
+    const res = await fetch("./publications.json");
+
+    if (!res.ok) {
+      throw new Error("HTTP error " + res.status);
+    }
+
     const data = await res.json();
+
+    data.sort((a, b) => b.year - a.year);
 
     container.innerHTML = "";
 
     data.forEach(pub => {
       const div = document.createElement("div");
-      div.className = "pub";
-
       div.innerHTML = `
         <b>${pub.title}</b><br>
         ${pub.authors}<br>
         <i>${pub.venue}</i>, ${pub.year}
         <hr>
       `;
-
       container.appendChild(div);
     });
 
   } catch (err) {
-    container.innerHTML = "Failed to load publications.";
+    console.error(err);
+    container.innerHTML = "⚠️ Failed to load publications. Check console (F12).";
   }
 }
 
